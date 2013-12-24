57a928b89ed487204655a18743d606513b04568a
William Corless <wmcorless@gmail.com>
2013-12-24 13:30:28 -1000

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("paccoin");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-PAC"
+#define CLIENT_VERSION_SUFFIX   "-beta"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
