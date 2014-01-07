f495a527d857b5dc1ffcd4e775cd021f30b0062f
William Corless <wmcorless@gmail.com>
2014-01-06 20:37:34 -1000

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -34,7 +34,6 @@ const std::string CLIENT_NAME("Satoshi");
 #endif
 
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
-#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
 #    define GIT_COMMIT_ID "$Format:%h$"
 #    define GIT_COMMIT_DATE "$Format:%cD"
